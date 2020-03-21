d7b823afc124d8df5f1cfa4bd4769c387bbcffe4
Johnathan R. Burgess <ryanburgess173@outlook.com>
2020-03-21 19:49:36 -0400

    deleted unnecessary SQL

--- src/main/sql/finance_management.sql
+++ src/main/sql/finance_management.sql
@@ -1,32 +1,13 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Mar 21, 2020 at 11:25 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `finance_management`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `accounts`
---
 
 CREATE TABLE `accounts` (
   `accountID` int(255) NOT NULL,
@@ -38,18 +19,9 @@ CREATE TABLE `accounts` (
   `transactions` text DEFAULT NULL COMMENT 'VarChar too small'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `accounts`
---
-
 INSERT INTO `accounts` (`accountID`, `username`, `balance`, `passwd`, `annualInterestRate`, `dateCreated`, `transactions`) VALUES
 (1, 'rburgess', 21366, 'root', 0.02, '2020-02-21 14:49:08', NULL);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `transactions`
---
 
 CREATE TABLE `transactions` (
   `transactionID` int(11) NOT NULL,
@@ -61,40 +33,16 @@ CREATE TABLE `transactions` (
   `balance` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `transactions`
---
 
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `accounts`
---
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`accountID`);
 
---
--- Indexes for table `transactions`
---
 ALTER TABLE `transactions`
   ADD PRIMARY KEY (`transactionID`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `accounts`
---
 ALTER TABLE `accounts`
   MODIFY `accountID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `transactions`
---
 ALTER TABLE `transactions`
   MODIFY `transactionID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=116;
 COMMIT;
