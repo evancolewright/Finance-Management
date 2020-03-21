1f0b1030576518bf211be74a1ebfd5685d3a494f
Evan Cole Wright <35124669+evancolewright@users.noreply.github.com>
2020-03-20 22:48:18 -0400

    Transaction persistence implemented

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -18,9 +18,9 @@ import java.sql.*;
 
 public class Controller {
 
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "";
-    private static final String CONNECTION = "jdbc:mysql://localhost/finance_management";
+    private final String USERNAME = "root";
+    private final String PASSWORD = "";
+    private final String CONNECTION = "jdbc:mysql://localhost/finance_management";
 
     //=================================================================================================================
     // Class Attributes
@@ -57,36 +57,45 @@ public class Controller {
     //=================================================================================================================
     @FXML
     public void handleLoginSubmit() throws SQLException {
-        String username = login_username_field.getText();
-        String query = "SELECT username, passwd FROM accounts WHERE username LIKE '"+username+"';";
-        try{
-            Connection con = DriverManager.getConnection(CONNECTION, USERNAME, PASSWORD);
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(query);
-            System.out.println(st);
-            System.out.println(rs);
-            if(rs.next()){
+        final String username = login_username_field.getText();
+        final String query = "SELECT username, passwd FROM accounts WHERE (username) LIKE (?)";
+        Connection connection = null;
+        PreparedStatement statement = null;
+        try {
+            connection = DriverManager.getConnection(CONNECTION, USERNAME, PASSWORD);
+            statement = connection.prepareStatement(query);
+            statement.setString(1, username);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
                 String password = rs.getString("passwd").toString();
-                if(password.equals(login_password_field.getText())){
-                    LoginSuccess();
-                    Stage primaryStage  = new Stage();
+                if (password.equals(login_password_field.getText())) {
+                    Stage primaryStage = new Stage();
                     Parent root = FXMLLoader.load(getClass().getResource("/FXML/Main.fxml"));
                     Scene scene = new Scene(root);
                     primaryStage.setScene(scene);
                     primaryStage.show();
-                }else{
+                } else {
                     LoginFailure();
                 }
             }
         } catch (SQLException | IOException exception) {
             System.out.println(exception.toString());
+        } finally {
+            //assure connection & statement closure regardless of exception
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
         }
     }
 
-    public void LoginSuccess(){
-        System.out.println("Login success!");
+    public void LoginSuccess(String username) throws IOException {
+        System.out.println("Login success! Welcome, " + username + "!");
     }
-    public void LoginFailure(){
+
+    public void LoginFailure() {
         System.out.println("Login Failure!");
     }
 
@@ -100,42 +109,69 @@ public class Controller {
     }
 
     @FXML
-    public void handleSubmitTransactionAction(){
-
+    public void handleSubmitTransactionAction() throws SQLException {
+        Connection connection = null;
+        PreparedStatement statement = null;
+        int id = new_account.getId();
         try {
-
             //by default data stored in a text field on the gui is stored as a string, so we're converting to a double
             double amount = Double.parseDouble(new_transaction_amount_field.getText());
+            ObservableList<TransactionView> viewable_data = tableView.getItems();
+
+            connection = DriverManager.getConnection(CONNECTION, USERNAME, PASSWORD);
+            String query = "INSERT INTO transactions (accountID, date, type, amount, description)" +
+                    " values (?, ?, ?, ?, ?)";
+
+            // PreparedStatement is going to save us creating a whole new statement dependent on transaction type.
+            // Set our initial query variables that we know up to this point.
+            statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            statement.setDate(2, new java.sql.Date(new java.util.Date().getTime()));
+            statement.setDouble(4, amount);
+            statement.setString(5, new_transaction_description_field.getText());
+
 
             if (new_transaction_type_choice.getSelectionModel().getSelectedItem().toString().equals("Deposit")) {
-
                 Transaction transaction = new_account.deposit(amount, new_transaction_description_field.getText());
-                ObservableList<TransactionView> viewable_data = tableView.getItems();
                 viewable_data.add(new TransactionView(String.valueOf(transaction.getType()), String.valueOf(transaction.getDate()),
                         String.valueOf(transaction.getAmount()), String.valueOf(transaction.getDescription()),
                         String.valueOf(transaction.getBalance())));
-
-            } else if (new_transaction_type_choice.getSelectionModel().getSelectedItem().toString().equals("Withdraw")) {
-
+                statement.setString(3, String.valueOf(transaction.getType()));
+            }
+            if (new_transaction_type_choice.getSelectionModel().getSelectedItem().toString().equals("Withdraw")) {
                 // checks to make sure the user has enough money in their account.
                 if (new_account.getBalance() >= amount) {
                     Transaction transaction = new_account.withdraw(amount, new_transaction_description_field.getText());
-                    ObservableList<TransactionView> data = tableView.getItems();
-                    data.add(new TransactionView(String.valueOf(transaction.getType()), String.valueOf(transaction.getDate()),
+                    viewable_data.add(new TransactionView(String.valueOf(transaction.getType()), String.valueOf(transaction.getDate()),
                             String.valueOf(transaction.getAmount()), String.valueOf(transaction.getDescription()),
                             String.valueOf(transaction.getBalance())));
-                } else {
+                    statement.setString(3, String.valueOf(transaction.getType()));
 
+                } else {
                     // if they don't display this error message:
                     // calls my private method alert passing it two arguments
                     alert(Alert.AlertType.ERROR, "There isn't enough money in your account to complete this transaction!");
                 }
             }
+            // execute our statement
+            statement.execute();
+
         } catch (NullPointerException exception) {
             alert(Alert.AlertType.ERROR, "Please enter a valid number in the amount box, make sure something is in description," +
                     "and make sure you have selected either deposit or withdraw for this transaction. Thank you!");
         } catch (NumberFormatException exception) {
             alert(Alert.AlertType.ERROR, "Please make sure you entered a valid number in the amount field!");
+        } catch (SQLException e) {
+            //TODO
+            System.out.print(e);
+            alert(Alert.AlertType.ERROR, "Database Error!");
+        } finally {
+            if (statement != null) {
+                statement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
         }
     }
 /*
@@ -161,6 +197,10 @@ public class Controller {
     }
     //================================================================================================================
 */
+
+    private void saveTransaction(char transactionType, double amount) {
+
+    }
 
     private void alert(Alert.AlertType alertType, String alertText) {
         Alert errorAlert = new Alert(alertType);
